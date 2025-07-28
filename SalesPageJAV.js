5a4709b19b9c7312dcba3d0822786a2ddfbd24c2
MakelYisrael <115165219+MakelYisrael@users.noreply.github.com>
2025-07-28 15:04:12 -0400

    Update SalesPageJAV.js

--- SalesPageJAV.js
+++ SalesPageJAV.js
@@ -260,7 +260,7 @@ function deleteProduct(index) {
     }
 }
 
-document.getElementById('addProductPage').addEventListener('click', showAddProductPage);
+document.getElementById('showAddProductPageBtn').addEventListener('click', showAddProductPage);
 
 // --- Edit Product Functionality ---
 function showEditProductPage(index) {
