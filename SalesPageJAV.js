7bb8246161c9427abb732a1cca77134d8e61c1f0
MakelYisrael <115165219+MakelYisrael@users.noreply.github.com>
2025-09-24 20:21:23 +0000

    Update SalesPageJAV.js

--- SalesPageJAV.js
+++ SalesPageJAV.js
@@ -227,7 +227,7 @@ document.getElementById('searchBar').addEventListener('input', filterProducts);
 // Add item to cart
 function addToCart(item) {
   // If the same product+color exists, increase quantity instead of duplicating
-    alert(item + " added to cart!");
+    alert(item.name + " added to cart!");
   const existing = cart.find(p => p.name === item.name && p.color === item.color);
   if (existing) {
     existing.quantity += item.quantity;
@@ -685,6 +685,7 @@ window.onload = async () => {
         document.getElementById('logoutBtn').style.display = 'inline-block';
     }*/
 };
+
 
 
 
